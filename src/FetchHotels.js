7e77be59b4c51e90a5925fc97c209c54ae8089cd
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-05-15 04:44:28 -0400

    Add booking Modal

--- src/FetchHotels.js
+++ src/FetchHotels.js
@@ -1,7 +1,11 @@
 import React from "react";
 import moment from "moment";
 
-import { Card, CardColumns, CardDeck, CardGroup, Button } from "react-bootstrap";
+import {
+  Card,
+  CardGroup,
+  Button,
+} from "react-bootstrap";
 
 class Booking extends React.Component {
   constructor(props) {
@@ -19,49 +23,83 @@ class Booking extends React.Component {
           {location.state.Guests} guests in {location.state.Rooms} rooms.
         </p>
 
-        <div
+        <CardGroup
           style={{
             display: "flex",
             flexDirection: "column",
             position: "relative",
-            left:"15%"
-            
+            left: "15%",
           }}
         >
-          <Card style={{  position: "realtive", left: "5%", width: "900px", height: "240px", cursor: "pointer"}}>
+          <Card
+            style={{
+              position: "realtive",
+              left: "5%",
+              width: "900px",
+              height: "240px",
+              cursor: "pointer",
+            }}
+          >
             <Card.Img
               variant="right"
-              style = {{width:"300px", height:"240px"}}
+              style={{ width: "300px", height: "240px" }}
               src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/1f/e0/a6/exterior-view.jpg?w=1300&h=-1&s=1"
             />
             <Card.Body>
-              <Card.Title style = {{position: 'absolute', left:"35%", bottom:"85%", color:'blue'}}>Card title</Card.Title>
-              <Card.Text style = {{position: 'absolute', left:"35%", bottom:"65%"}}>
+              <Card.Title
+                style={{
+                  position: "absolute",
+                  left: "35%",
+                  bottom: "85%",
+                  color: "blue",
+                }}
+              >
+                Card title
+              </Card.Title>
+              <Card.Text
+                style={{ position: "absolute", left: "35%", bottom: "65%" }}
+              >
                 This is a wider card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
                 longer. <Button> View Deal</Button>
               </Card.Text>
-              
             </Card.Body>
-            
           </Card>
-          <Card style={{  position: "realtive", left: "5%", width: "900px", height: "240px", cursor: "pointer"}}>
+          <Card
+            style={{
+              position: "realtive",
+              left: "5%",
+              width: "900px",
+              height: "240px",
+              cursor: "pointer",
+            }}
+          >
             <Card.Img
               variant="right"
-              style = {{width:"300px", height:"240px"}}
+              style={{ width: "300px", height: "240px" }}
               src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/a0/18/31/exterior-view.jpg?w=1300&h=-1&s=1"
             />
             <Card.Body>
-              <Card.Title style = {{position: 'absolute', left:"35%", bottom:"85%", color:'blue'}}>Card title</Card.Title>
-              <Card.Text style = {{position: 'absolute', left:"35%", bottom:"65%"}}>
+              <Card.Title
+                style={{
+                  position: "absolute",
+                  left: "35%",
+                  bottom: "85%",
+                  color: "blue",
+                }}
+              >
+                Card title
+              </Card.Title>
+              <Card.Text
+                style={{ position: "absolute", left: "35%", bottom: "65%" }}
+              >
                 This is a wider card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
                 longer.
               </Card.Text>
             </Card.Body>
-            
           </Card>
-        </div>
+        </CardGroup>
       </div>
     );
   }
